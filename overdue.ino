feea5d3f6937f838f2ed204db757f216e85162a9
RuitaoLiang <35542361+RuitaoLiang@users.noreply.github.com>
2018-03-11 16:49:14 -0700

    Create overdue.ino

---
+++ overdue.ino
@@ -0,0 +1,43 @@
+int sensorpin1 = A0; 
+int ledpin1 = 5;
+int sensorValue = 0;
+int ledpin2 = 9;
+int ledpin3 = 6;
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(ledpin1,OUTPUT);
+  pinMode(ledpin2,OUTPUT);
+  pinMode(ledpin3,OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  sensorValue = analogRead(sensorpin1);
+  Serial.println(sensorValue);
+  if(sensorValue>257.5)
+  for (int brightness = 0; brightness < 255; brightness++) {
+      analogWrite(ledpin1, brightness);
+      delay(2);
+  
+  if(sensorValue>515) 
+  for (int brightness = 255; brightness >= 0; brightness--) {
+      analogWrite(ledpin2, brightness);
+      delay(2);
+  
+ if(sensorValue>772.5)
+  analogWrite(ledpin3, HIGH);
+  delay(sensorValue);
+   
+  // turn the ledPin off:
+ if(sensorValue<257.5)analogWrite(ledpin1, LOW);
+  if(sensorValue<515)digitalWrite(ledpin2, LOW);
+  if(sensorValue<772.5)digitalWrite(ledpin3, LOW);
+// stop the program for for <sensorValue> milliseconds:
+delay(sensorValue);
+}
+  }
+}
